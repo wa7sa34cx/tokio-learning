09a1dc5c5480914a588a91e684026eb1d412d928
Alex Black <wa7@sa34.cx>
2021-08-27 19:03:13 +0300

    first commit

---
+++ examples/hello-redis.rs
@@ -0,0 +1,21 @@
+use mini_redis::{client, Result};
+// use std::str;
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    // let s = "hello".into();
+
+    client.set("hello", "world".into()).await?;
+
+    let result = client.get("hello").await?;
+
+    // if let Some(val) = result.unwrap() {
+    //     println!("The result is {:?}", str::from_utf8(val));
+    // }
+
+    println!("Got value from the server; result={:?}", result);
+
+    Ok(())
+}
